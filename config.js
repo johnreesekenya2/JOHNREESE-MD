11034ff476bfaaf22eb47d1928193ee07b472247
johnreesekenya2 <johnreeserwanda@gmail.com>
2025-08-08 20:10:23 +0300

    Add config.js - JOHNREESE-MD Bot

---
+++ config.js
@@ -0,0 +1,32 @@
+// LEGACY XMD Bot Configuration
+// Created by John Reese
+
+const config = {
+    SESSION_ID: process.env.SESSION_ID || "JOHNREESE-MD~6MFwnTzR#YG0tLwmhTtpkGCBvhSWDoftSMcZ6cnu7NUK-2XVQ488",
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY JOHNREESE-MD 🤍*",
+    WELCOME: process.env.WELCOME || "true",
+    ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    MENTION_REPLY: process.env.MENTION_REPLY || "false",
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/45jhow.png",
+    PREFIX: process.env.PREFIX || ".",
+    BOT_NAME: process.env.BOT_NAME || "JOHNREESE-MD",
+    STICKER_NAME: process.env.STICKER_NAME || "JOHNREESE-MD",
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
+    DELETE_LINKS: process.env.DELETE_LINKS || "false",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "254745282166",
+    OWNER_NAME: process.env.OWNER_NAME || "JOHN REESE",
+    DESCRIPTION: process.env.DESCRIPTION || "*almost to perfection*",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/45jhow.png",
+    LIVE_MSG: process.env.LIVE_MSG || "*JOHNREESE-MD IS ALIVE AND KICKING*⚡",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    MODE: process.env.MODE || "public"
+};
+
+module.exports = config;
